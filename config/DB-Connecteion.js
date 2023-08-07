e8c4c8eb0c36d4f58ded0e5d6c62b1b075bd19da
Saleh Hadi Aburah <sasa97977s@hotmail.com>
2023-08-07 14:46:27 +0300

    connect with Altlas database

--- config/DB-Connecteion.js
+++ config/DB-Connecteion.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 const connectWithDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      `mongodb+srv://sasa97977s:${process.env.DB_PASS}@cluster0.ycpox6y.mongodb.net/Almanar-Backend?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("DB GOT CONNECTED");
   } catch (error) {
     console.log("DB Conteccted Issue");
